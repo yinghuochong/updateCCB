ccfa99936568fd93558474e48c65d2e793e130a1
Lihua Liu <wpfun@foxmail.com>
2015-03-22 10:48:18 +0800

    Initial Commit

---
+++ updateCCB/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  updateCCB
+//
+//  Created by liulihua on 15/3/22.
+//  Copyright (c) 2015年 firefly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
